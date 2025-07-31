9eb0c013ad8d69e233386e75375cd2c1e0382d88
unknown <dbaxtiyorov2815@gmail.com>
2025-07-31 15:37:15 +0500

    upload post done

--- main/views.py
+++ main/views.py
@@ -1,15 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from users.models import UserProfile
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Post
+from django.contrib import messages
 
 class HomePageView(LoginRequiredMixin, View):
     login_url = "/auth/login/"
     template_name = 'index.html'
     def get(self, request):
         
-        posts = Post.objects.all()
+        posts = Post.objects.all()[::-1]
         profiles = UserProfile.objects.all()
         user = request.user
         profile = UserProfile.objects.get(user=user)
@@ -32,4 +33,18 @@ class PostUpload(LoginRequiredMixin, View):
         pass
     def post(self, request, *args, **kwargs):
         image = request.FILES.get('image')
-        print(image)
+        desc = request.POST.get("caption")
+        is_exist = Post.objects.filter(image=image, desc=desc).exists()
+        if is_exist:
+            user = Post.objects.get(image=image, desc=desc).user
+
+            messages.warning(request=request, message=f"This post already exists !!! posted by {user.username}")
+        else:
+            user = request.user
+            post = Post.objects.create(user=user, image=image, desc=desc)
+            post.save()
+
+            messages.success(request=request, message="Post has been created succesfully")
+
+        return redirect('home')
+            
